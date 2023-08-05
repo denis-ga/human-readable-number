857388c65760891f1a7444f592be084701f1434e
Denis G <denis.g@engagecraft.com>
2023-08-05 11:50:00 +0300

    feat: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+const digitals = require("./enums/dictionary");
+
 module.exports = function toReadable (number) {
-  
+    switch(true){
+        case number < 20:
+            return digitals[number];
+        case number < 100:
+            const tenths = Math.floor(number/10) * 10;
+            const num = number%10;
+            return `${digitals[tenths]}${num === 0 ? '' : ' ' + digitals[num]}`;
+        case number < 1000:
+            const hundredths = Math.floor(number/100);
+            const _tenths = number % 100;
+            const readableTenths = _tenths === 0 ? '' : ' ' +  toReadable(_tenths);
+            return `${digitals[hundredths]} hundred${readableTenths}`;
+    }
 }
